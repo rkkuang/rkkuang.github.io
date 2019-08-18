7d420c5b253e7cab8b5d939cccec191531befabe
rkkuang <renkunkuang@gmail.com>
2019-08-18 22:55:31 +0800

    update

--- upload.sh
+++ upload.sh
@@ -2,6 +2,6 @@ git add --all
 git commit -m "update"
 git push -u origin master
 
-git checkout source
-git push origin source
-git checkout master
+#git checkout source
+#git push origin source
+#git checkout master
