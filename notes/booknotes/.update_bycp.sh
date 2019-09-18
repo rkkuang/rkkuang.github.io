f8d96c4e5ccb51fdd7d84ded64173c28c73c8fe0
rkkuang <renkunkuang@gmail.com>
2019-09-18 09:07:14 +0800

    update

---
+++ notes/booknotes/.update_bycp.sh
@@ -0,0 +1,17 @@
+cp ~/THU/astro/notes/difmap/difmap.pdf .
+cp ~/THU/astro/notes/AnIntro2ModernAstroph/AnIntro2ModernAstroph.pdf .
+cp ~/THU/astro/notes/ska_mwa_lofar/radio_frontedge_scigoals.pdf .
+cp ~/THU/astro/notes/essential_radio_astronomy/essential_radio_astronomy_lecture_notes.pdf .
+cp ~/THU/astro/notes/landau/landau.pdf .
+cp ~/THU/astro/notes/脉冲星物理/pulsar_physics.pdf .
+cp ~/THU/astro/notes/SynthesisImagingInRadioAstronomy/SynthesisImagingInRadioAstronomy.pdf .
+cp ~/THU/astro/notes/天体物理概论/天体物理概论.pdf . 
+
+
+
+
+
+
+
+
+
