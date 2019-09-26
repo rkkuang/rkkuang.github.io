a5963b9734e56df40b791fe8623cddf9938adb64
rkkuang <renkunkuang@gmail.com>
2019-09-26 20:34:07 +0800

    update

---
+++ .update_bycp.sh
@@ -0,0 +1,19 @@
+cp ~/THU/astro/notes/difmap/difmap.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/AnIntro2ModernAstroph/AnIntro2ModernAstroph.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/ska_mwa_lofar/radio_frontedge_scigoals.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/essential_radio_astronomy/essential_radio_astronomy_lecture_notes.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/landau/landau.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/脉冲星物理/pulsar_physics.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/SynthesisImagingInRadioAstronomy/SynthesisImagingInRadioAstronomy.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/天体物理概论/天体物理概论.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+cp ~/THU/astro/notes/umrao/umrao.pdf ~/Documents/rkkuang.github.io/notes/booknotes/umrao.pdf
+cp ~/THU/astro/notes/Tools4RA/Tools4Radio_Astronomy.pdf ~/Documents/rkkuang.github.io/notes/booknotes/Tools4Radio_Astronomy.pdf
+cp ~/THU/astro/notes/天文学革命-仰望星空40年/TheAstronomyRevolution.pdf ~/Documents/rkkuang.github.io/notes/booknotes/TheAstronomyRevolution.pdf
+cp ~/THU/astro/notes/射电天文工具/射电天文工具.pdf ~/Documents/rkkuang.github.io/notes/booknotes/
+
+cp ~/Documents/me/CV/cv1908/cv1908.pdf ~/Documents/rkkuang.github.io/cv.pdf 
+
+
+
+
+
