882b46ad970b438f662c80853b0fc27837e96ee5
rkkuang <renkunkuang@gmail.com>
2019-09-28 11:54:48 +0800

    update

--- .uphtml.sh
+++ .uphtml.sh
@@ -2,7 +2,7 @@
 
 #pandoc --toc
 
-pandoc -f markdown --toc  -o notes/index.html notes/index.md
+pandoc -f markdown -s --toc -c theme/css/pandoc.css  -o notes/index.html notes/index.md
 
-pandoc -f markdown --toc  -o notes/seminar_notes/seminar_notes.html notes/seminar_notes/seminar_notes.md
+pandoc -f markdown -s --toc -c theme/css/pandoc.css  -o notes/seminar_notes/seminar_notes.html notes/seminar_notes/seminar_notes.md
 
