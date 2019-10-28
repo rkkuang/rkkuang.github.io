bed797560b6965a46de09254b312941d0f24a8b1
anything <renkunkuang@gmail.com>
2019-10-28 20:49:08 +0800

    update

--- .uphtml.sh
+++ .uphtml.sh
@@ -12,7 +12,7 @@ pandoc -f markdown -s --toc -c ../theme/css/pandoc.css  -o research/research.htm
 pandoc -f markdown -s --toc -c ../theme/css/pandoc.css  -o notes/index.html notes/index.md
 pandoc -f markdown -s --toc -c ../theme/css/pandoc.css  -o notes/collections.html notes/collections.md
 
-pandoc -f markdown -s --toc -c ../theme/css/pandoc.css  -o notes/seminar_notes/seminar_notes.html notes/seminar_notes/seminar_notes.md
+pandoc -f markdown -s --toc -c ../../theme/css/pandoc.css  -o notes/seminar_notes/seminar_notes.html notes/seminar_notes/seminar_notes.md
 
 pandoc -f markdown -s --toc -c ../../theme/css/pandoc.css -o notes/blogs/blogs.html notes/blogs/blogs.md
 
